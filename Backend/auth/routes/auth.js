a5044f4cf97cb93c53869fad53d94b8f5222f1a6
Aman-63 <amanraj5703@gmail.com>
2025-10-04 22:48:20 +0530

    Added login signup features

--- Backend/auth/routes/auth.js
+++ Backend/auth/routes/auth.js
@@ -1,9 +1,33 @@
-import express, { response } from "express";
+import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import pool from "../db.js";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+
+dotenv.config();
 
 const router = express.Router();
+
+// send Mail for verification
+
+const transporter = nodemailer.createTransport(
+  {
+    service: "Gmail",
+    auth:{
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASS
+    }
+  }
+);
+
+async function sendEmail(to, subject, html){
+  await transporter.sendMail({
+    from: process.env.USER_EMAIL,
+    to, subject, html
+  });
+}
 
 // signup
 router.post("/signup", async (req, res) => {
@@ -21,20 +45,47 @@ router.post("/signup", async (req, res) => {
     if(rows.length>0){
       return res.status(400).json({message: "User already exists"});
     }
-    
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await pool.query(
-      "INSERT INTO users (name, email, role, password) VALUES (?, ?, ?, ?)",
+    const [data] = await pool.query(
+      "INSERT INTO users (name, email, role, password, is_Verified) VALUES (?, ?, ?, ?, 0)",
       [name, email, role, hashedPassword]
     );
 
-    res.status(201).json({ message: "User registered" });
+    const userId = data.insertId;
+
+    const token = jwt.sign({id: userId}, process.env.JWT_SECRET, {expiresIn : "1h"});
+    const verifyLink = `http://localhost:5000/auth/verify/${token}`;
+
+    await sendEmail(
+      email,
+      "Verify your account",
+      `<p>Hello, ${name}, </p>
+      <p> Click here to verify your account: </p>
+      <a href ="${verifyLink}">${verifyLink}</a>`
+    );
+
+    res.status(201).json({ message: "Signup successful. Please verify you email" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
+
+// email Verification
+
+router.get("/verify/:token", async(req, res) =>{
+  try{
+    const {token} = req.params;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    await pool.query("UPDATE users SET is_verified = 1 WHERE id = ?", [decoded.id]);
+
+    res.redirect("/final.html");
+  }catch(err){
+    res.send("Email verification failed");
+  }
+})
 
 // login
 router.post("/login", async (req, res) => {
--- Backend/auth/server.js
+++ Backend/auth/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../../FrontEnd/Auth")));
 
 app.use(express.json());
 app.use(cookieParser());
